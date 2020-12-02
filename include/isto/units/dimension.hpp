3820536d6b528767da372b84eed811fa166446c7
Emmanuel LE TRONG <emmanuel.le-trong@cnrs-orleans.fr>
2020-12-02 11:02:48 +0100

    Make isto::hash_combine a mandatory dependency
    Having it optional was stupid as we use std::unordered_maps in our own
    header.

--- include/isto/units/dimension.hpp
+++ include/isto/units/dimension.hpp
@@ -1,11 +1,8 @@
 #pragma once
 #include <array>
-#if __has_include(<isto/hash_combine/hash_combine.hpp>)
 #include <isto/hash_combine/hash_combine.hpp>
-#define ISTO_UNITS_HAS_HASH_COMBINE
     using isto::hash_combine::hash_combine;
     using isto::hash_combine::hash_combine_range;
-#endif
 
     namespace
 isto::units
@@ -119,7 +116,6 @@ operator - (dimension_t a, dimension_t b) = delete;
 
 } // namespace isto::units
 
-#ifdef ISTO_UNITS_HAS_HASH_COMBINE
     namespace
 std
 {
@@ -136,4 +132,3 @@ std
         }
     };
 }
-#endif
