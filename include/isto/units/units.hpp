b373678b6b6536101c0f2595f2f60b08d82ed9d6
Emmanuel LE TRONG <emmanuel.le-trong@cnrs-orleans.fr>
2021-06-03 11:19:46 +0200

    Add base_unit_symbol

--- include/isto/units/units.hpp
+++ include/isto/units/units.hpp
@@ -9,4 +9,37 @@
 
 #include "detail/generated_units.inc"
 
+
+// Wait for sts::string to be constexpr
+    /*constexpr*/ inline auto
+base_unit_symbol (dimension_t const& d)
+{
+        using std::to_string;
+        static /*constexpr*/ auto
+    unit_symbol = std::array 
+    {
+          "m"sv
+        , "kg"sv
+        , "s"sv
+        , "A"sv
+        , "K"sv
+        , "mol"sv
+        , "cd"sv
+    };
+        /*constexpr*/ auto
+    r = std::string {};
+        /*constexpr*/ auto
+    need_a_dot = false;
+    for (std::size_t i = 0; i < dimension_t::count; ++i)
+    {
+        if (d[i] == 0) continue;
+        if (need_a_dot) r.push_back ('.');
+        r.append (unit_symbol[i]);
+        need_a_dot = true;
+        if (d[i] == 1) continue;
+        r.append (fmt::format ("{:g}", d[i]));
+    }
+    return r;
+}
+
 } // namespace isto::units
